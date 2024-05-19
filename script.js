17c98e4ecf5d6d2b92c8ab663e4209bfe5a5941c
abdul-basithm <abdulbasith88@gmail.com>
2024-05-19 18:56:24 +0530

    dom task on session

---
+++ script.js
@@ -0,0 +1,13 @@
+function myclick(){
+    var firstname= document.getElementById("firstname").value;
+    var middlename= document.getElementById("middlename").value;
+    var lastname= document.getElementById("lastname").value;
+    var email= document.getElementById("email").value;
+    var mob_num= document.getElementById("mob_num").value;
+    console.log(firstname);
+    console.log(middlename);
+    console.log(lastname);
+    console.log(email);
+    console.log(mob_num);
+
+}
